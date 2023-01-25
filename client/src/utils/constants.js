7eb2483b2defa4809d60a1d540b43554802b3ab4
Ryan Lee <ryan.legit config --global user.name Your>
2023-01-25 15:44:14 -0500

    moved client folder

--- client/src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-//0x69B8039e219fC05C0217dfb929304322412B3d8A
-
-import abi from "./Transactions.json";
-
-export const contractAddress = "0x69B8039e219fC05C0217dfb929304322412B3d8A";
-export const contractABI = abi.abi;
